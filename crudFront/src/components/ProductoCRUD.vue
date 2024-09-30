d3ccf3503dbe4c9af4b4885e0526eaa455296a91
camipaula <camilapaulavegatandazo29@gmail.com>
2024-09-30 09:10:28 -0500

    Initial commit

---
+++ crudFront/src/components/ProductoCRUD.vue
@@ -0,0 +1,108 @@
+<template>
+  <div class="container mt-4">
+    <h2 class="mb-4 text-center">Lista de Productos</h2>
+    <table class="table table-striped table-hover">
+      <thead class="thead-dark">
+        <tr>
+          <th>Nombre</th>
+          <th>Descripción</th>
+          <th>Precio</th>
+          <th>Acciones</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="producto in productos" :key="producto.id">
+          <td>{{ producto.nombre }}</td>
+          <td>{{ producto.descripcion }}</td>
+          <td>{{ producto.precio }}</td>
+          <td>
+            <button class="btn btn-primary btn-sm me-2" @click="editarProducto(producto)">Editar</button>
+            <button class="btn btn-danger btn-sm" @click="eliminarProducto(producto.id)">Eliminar</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+
+    <h3 class="mt-5">Crear/Editar Producto</h3>
+    <form @submit.prevent="guardarProducto" class="row g-3 mt-3">
+      <div class="col-md-4">
+        <input v-model="producto.nombre" class="form-control" placeholder="Nombre" required />
+      </div>
+      <div class="col-md-4">
+        <input v-model="producto.descripcion" class="form-control" placeholder="Descripción" required />
+      </div>
+      <div class="col-md-4">
+        <input v-model="producto.precio" class="form-control" type="number" placeholder="Precio" required />
+      </div>
+      <div class="col-12">
+        <button type="submit" class="btn btn-success">Guardar</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      productos: [],
+      producto: {
+        id: null,
+        nombre: '',
+        descripcion: '',
+        precio: 0,
+      },
+    };
+  },
+  mounted() {
+    this.cargarProductos();
+  },
+  methods: {
+    cargarProductos() {
+      axios.get('http://localhost:5170/api/productos')
+        .then(response => {
+          this.productos = response.data;
+        });
+    },
+    guardarProducto() {
+      if (this.producto.id) {
+        axios.put(`http://localhost:5170/api/productos/${this.producto.id}`, this.producto)
+          .then(() => {
+            this.cargarProductos();
+            this.resetFormulario();
+          });
+      } else {
+        const nuevoProducto = {
+          nombre: this.producto.nombre,
+          descripcion: this.producto.descripcion,
+          precio: this.producto.precio
+        };
+        axios.post('http://localhost:5170/api/productos', nuevoProducto)
+        .then(() => {
+          this.cargarProductos();
+          this.resetFormulario();
+        });
+      }
+    },
+    editarProducto(producto) {
+      this.producto = { ...producto };
+    },
+    eliminarProducto(id) {
+      axios.delete(`http://localhost:5170/api/productos/${id}`)
+        .then(() => {
+          this.cargarProductos();
+        });
+    },
+    resetFormulario() {
+      this.producto = {
+        id: null,
+        nombre: '',
+        descripcion: '',
+        precio: 0,
+      };
+    },
+  },
+};
+</script>
---
+++ crudFront/src/views/ProductView.vue
@@ -0,0 +1,20 @@
+<!-- src/views/ProductView.vue -->
+<template>
+  <div>
+    <ProductoCrud />
+  </div>
+</template>
+
+<script>
+import ProductoCrud from '../components/ProductoCRUD.vue';
+
+export default {
+  components: {
+    ProductoCrud
+  }
+};
+</script>
+
+<style scoped>
+/* Puedes agregar aquí el estilo que desees para el componente ProductoCrud */
+</style>
